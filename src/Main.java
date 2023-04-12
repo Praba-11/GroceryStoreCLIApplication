1cb518e16a4274d6adf9263bc2db474fbd2b7e24
Praba-11 <sukumarprabakaran@gmail.com>
2023-04-12 18:28:32 +0530

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,24 @@ import com.billing.app.domain.entity.Product;
 import com.billing.app.domain.entity.repository.jdbc.JdbcProductDAO;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
         // Creating Product Object
-        Product product = new Product("101r", "banana", "kg", "fruits", 10);
+        Product product = new Product("101vvv", "apple", "kg", "fruits", 10);
 
         JdbcProductDAO jdbcProductDAO = new JdbcProductDAO();
 
-        jdbcProductDAO.delete("101b");
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("name");
+        arrayList.add("water");
+        arrayList.add("unitCode");
+        arrayList.add("l");
+        arrayList.add("type");
+        arrayList.add("drinks");
+
+        jdbcProductDAO.edit("101a", arrayList);
     }
 }
--- src/com/billing/app/domain/entity/repository/jdbc/JdbcProductDAO.java
+++ src/com/billing/app/domain/entity/repository/jdbc/JdbcProductDAO.java
@@ -2,14 +2,17 @@ package com.billing.app.domain.entity.repository.jdbc;
 
 import com.billing.app.domain.entity.Product;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class JdbcProductDAO implements ProductDAO {
 
     @Override
     public void create(Product product) throws SQLException, ClassNotFoundException {
 
-        // Storing Product in Database
+        // Storing Product in Database table
         ConnectionDB connectionDB = new ConnectionDB();
         String sql = "INSERT INTO products (code, name, unit_code, type, price, stock) VALUES (?, ?, ?, ?, ?, ?)";
         PreparedStatement preparedStatement = connectionDB.getConnection().prepareStatement(sql);
@@ -27,14 +30,21 @@ public class JdbcProductDAO implements ProductDAO {
     }
 
     @Override
+    public void edit(String code, ArrayList arrayList) throws SQLException, ClassNotFoundException {
+
+        ConnectionDB connectionDB = new ConnectionDB();
+
+    }
+
+
+    @Override
     public void delete(String code) throws SQLException, ClassNotFoundException {
 
-        // Delete Product in Database
+        // Delete Product in Database table
         ConnectionDB connectionDB = new ConnectionDB();
         String sql = "DELETE FROM products WHERE code = '" + code + "'";
         PreparedStatement preparedStatement = connectionDB.getConnection().prepareStatement(sql);
         preparedStatement.executeUpdate();
-
         preparedStatement.close();
 
         System.out.println("Product deleted.");
@@ -43,30 +53,102 @@ public class JdbcProductDAO implements ProductDAO {
     @Override
     public void list(int range) throws SQLException, ClassNotFoundException {
 
+        // List's range of Products as default in Database table
         ConnectionDB connectionDB = new ConnectionDB();
+        String query = "SELECT * FROM products LIMIT '" + range + "'";
+        Statement statement = connectionDB.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
 
+        while (resultSet.next()) {
+            String code = resultSet.getString(1);
+            String name = resultSet.getString(2);
+            String unitCode = resultSet.getString(3);
+            String type = resultSet.getString(4);
+            float price = resultSet.getFloat(5);
+            int stock = resultSet.getInt(6);
+            System.out.println(code + ": " + name + ": " + unitCode + ": " + type + ": " + price + ": " + stock);
+        }
 
     }
 
     @Override
     public void list(int range, int page) throws SQLException, ClassNotFoundException {
 
+        // List's range of Products by pagination in Database table
+        ConnectionDB connectionDB = new ConnectionDB();
+        String query = "SELECT * FROM products OFFSET '" + (range * (page - 1)) + "'" + "LIMIT '" + range + "'";
+        Statement statement = connectionDB.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+        while (resultSet.next()) {
+            String code = resultSet.getString(1);
+            String name = resultSet.getString(2);
+            String unitCode = resultSet.getString(3);
+            String type = resultSet.getString(4);
+            float price = resultSet.getFloat(5);
+            int stock = resultSet.getInt(6);
+            System.out.println(code + ": " + name + ": " + unitCode + ": " + type + ": " + price + ": " + stock);
+        }
+
     }
 
     @Override
     public void list(String searchText) throws SQLException, ClassNotFoundException {
+
+        // Search for instances of searchText in Database table
+        ConnectionDB connectionDB = new ConnectionDB();
+        String query = "SELECT * FROM products WHERE code || name || unit_code || type || price || stock LIKE '%" + searchText + "%'";
+        Statement statement = connectionDB.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+        while (resultSet.next()) {
+            String code = resultSet.getString(1);
+            String name = resultSet.getString(2);
+            String unitCode = resultSet.getString(3);
+            String type = resultSet.getString(4);
+            float price = resultSet.getFloat(5);
+            int stock = resultSet.getInt(6);
+            System.out.println(code + ": " + name + ": " + unitCode + ": " + type + ": " + price + ": " + stock);
+        }
 
     }
 
     @Override
     public void list(String attribute, String searchText) throws SQLException, ClassNotFoundException {
 
+        // Search for instances of searchText using attribute in the Database table
+        ConnectionDB connectionDB = new ConnectionDB();
+        String query = "SELECT * FROM products WHERE " + attribute + " LIKE '%" + searchText + "%'";
+        Statement statement = connectionDB.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+        while (resultSet.next()) {
+            String code = resultSet.getString(1);
+            String name = resultSet.getString(2);
+            String unitCode = resultSet.getString(3);
+            String type = resultSet.getString(4);
+            float price = resultSet.getFloat(5);
+            int stock = resultSet.getInt(6);
+            System.out.println(code + ": " + name + ": " + unitCode + ": " + type + ": " + price + ": " + stock);
+        }
+
     }
 
     @Override
     public void list(String attribute, String searchText, int range, int page) throws SQLException, ClassNotFoundException {
 
-    }
+        // Search for instances of searchText using attribute, range and pagination
+        ConnectionDB connectionDB = new ConnectionDB();
+        String query = "SELECT * FROM (SELECT * FROM products OFFSET " + range + " * (" + page + " - 1) LIMIT " + range + " ) subquery WHERE " + attribute + " = '" + searchText + "'";
+        Statement statement = connectionDB.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+        while (resultSet.next()) {
+            String code = resultSet.getString(1);
+            String name = resultSet.getString(2);
+            String unitCode = resultSet.getString(3);
+            String type = resultSet.getString(4);
+            float price = resultSet.getFloat(5);
+            int stock = resultSet.getInt(6);
+            System.out.println(code + ": " + name + ": " + unitCode + ": " + type + ": " + price + ": " + stock);
+        }
 
+    }
 
 }
